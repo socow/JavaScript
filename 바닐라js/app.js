527ae59d342b9374ce69705fbbc6f49640eaf4f7
so jaehyun <105201721+socow@users.noreply.github.com>
2022-07-08 04:04:15 +0900

    오늘 공부한이벤트

--- 바닐라js/app.js
+++ 바닐라js/app.js
@@ -1,1 +1,23 @@
-alert("hi");
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  h1.style.color = "blue";
+}
+
+function handleMouseenter() {
+  h1.innerText = "mouse is here!";
+}
+
+function handleMousLeave() {
+  h1.style.color = "red";
+  h1.innerText = "mouse is gone! ";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseenter);
+h1.addEventListener("mouseleave", handleMousLeave);
+
+window.addEventListener("resize", handleWindowResize);
